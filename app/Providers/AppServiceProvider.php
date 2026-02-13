09b7ff906c85d27a3ea82fb279debb4fc4f3b4dc
Nicolas Figueredo <gtyt@outlook.com.ar>
2026-02-13 09:43:10 -0300

    cambios 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,8 +33,8 @@ class AppServiceProvider extends ServiceProvider
                 'logos' => Logos::first()
             ]);
         });
-        // if (app()->environment('production')) {
-        // URL::forceScheme('https');
-        // }
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+        }
     }
 }
